f90a5722b7270f34ffb62683f9afbf2e3b370804
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2022-12-11 21:41:51 +0100

    Modificamos findByPatient Post->Get

--- src/consults/consults.controller.ts
+++ src/consults/consults.controller.ts
@@ -33,7 +33,7 @@ export class ConsultsController {
     return await this.consultsService.find(findConsultDto);
   }
 
-  @Post('findByPatient/:id')
+  @Get('findByPatient/:id')
   async findByPatient (@Param('id') id: string) {
     return await this.consultsService.findByPatient(id);
   }
