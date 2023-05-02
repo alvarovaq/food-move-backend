2a7deb0ebfc2e3d53872d85e6b80e0a661c3254f
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2023-05-02 15:26:10 +0200

    Arreglamos problema de generar una contraseña aleatoria

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,7 +10,8 @@ export function newRandomPassword (length: number = 8) {
   const abecedario = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9",".","-","_","$","&","#","@"];
   let password = "";
   for (let i = 0; i < length; i++) {
-    const randomNumber = Math.round(Math.random()*abecedario.length);
+    let randomNumber = Math.round(Math.random()*abecedario.length);
+    if (randomNumber == abecedario.length) randomNumber = 0;
     password += abecedario[randomNumber];
   }
   return password;
